e436032a1ed74d9f76266e05c0e6fde6443128e0
tech08mag <139458159+Tech08mag@users.noreply.github.com>
2025-12-04 17:01:54 +0000

    started with sessions

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
     return render_template('index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -13,6 +15,9 @@ def register():
     
 @app.route('/login', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
     return render_template('login.html')
 
 @app.route('/settings', methods=['GET', 'POST'])
